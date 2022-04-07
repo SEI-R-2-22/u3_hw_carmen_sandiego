8a38c02d93e8d1e0058b69294b7fa7814cedf6fe
GJ Haaland <haaland.gj@gmail.com>
2022-04-07 17:24:38 -0400

    Group 2 GJ Haaland

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,37 +1,73 @@
+
+
+city = (id, name, countrycode, district, population)
+country = (code, name, continent, region, surfacearea, indepyear, population, lifeexpectancy, gnp, gnpold, localname, governmentform, headofstate, capital, code2) 
+countrylanguage = (countrycode, "language", isofficial, percentage)
+
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+SELECT name FROM country WHERE region = 'Southern Europe' ORDER BY population LIMIT 1
+
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
+SELECT * FROM countrylanguage WHERE contrycode = 'VAT' 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
+SELECT name FROM countrylanguage JOIN country ON countrylanguage.countrycode = country.code WHERE language = 'Italian' AND region = 'Southern Europe' AND percentage = 100;
+
+ name    
+------------
+ San Marino
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
+SELECT * FROM city WHERE countrycode = 'SMR' AND NOT name = 'San Marino';
+
+ id  |    name    | countrycode |     district      | population 
+------+------------+-------------+-------------------+------------
+ 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+SELECT * FROM country JOIN city ON country.code = city.countrycode WHERE continent = 'South America' AND city.name LIKE 'Serr%';
+
+ code |  name  |   continent   |    region     | surfacearea  | indepyear | population | lifeexpectancy |    gnp    |  gnpold   | localname |  governmentform  |        headofstate        | capital | code2 | id  | name  | countrycode |     district     | population 
+------+--------+---------------+---------------+--------------+-----------+------------+----------------+-----------+-----------+-----------+------------------+---------------------------+---------+-------+-----+-------+-------------+------------------+------------
+ BRA  | Brazil | South America | South America | 8.547403e+06 |      1822 |  170115000 |           62.9 | 776739.00 | 804108.00 | Brasil    | Federal Republic | Fernando Henrique Cardoso |     211 | BR    | 265 | Serra | BRA         | Espï¿½rito Santo |     302666
+(1 row)
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
+SELECT name FROM city WHERE id = 211;
 
+    name    
+------------
+ Brasï¿½lia
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
  -- landing dock HINT:Look at number 8.
 
+LOOK AT ANSWER 8 BELOW
+
 -- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
+
+SELECT * FROM city WHERE population = 91084;
+
+  id  |     name     | countrycode |  district  | population 
+------+--------------+-------------+------------+------------
+ 4060 | Santa Monica | USA         | California |      91084
 
 -- Our playdate of late has been unusually fun –
 -- As an agent, I'll say, you've been a joy to outrun.
@@ -45,4 +81,4 @@
 
 
 
--- She's in ____________________________!
+She's in SANTA MONICA!
